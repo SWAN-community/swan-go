26a8e28ebd0408998ddccb201ce489f6bd612a0c
James Rosewell <james@51degrees.com>
2020-11-18 19:05:43 +0000

    BUG: Preferences were not being shown.

--- handlers.go
+++ handlers.go
@@ -47,7 +47,7 @@ func AddHandlers(
 	http.HandleFunc("/swan/api/v1/update", handlerUpdate(s))
 	http.HandleFunc("/swan/api/v1/decode-as-json", handlerDecodeAsJSON(s))
 	http.HandleFunc("/swan/api/v1/create-offer-id", handlerCreateOfferID(s))
-	http.HandleFunc("/swan/preferences", handlerCapture(s))
+	http.HandleFunc("/swan/preferences/", handlerCapture(s))
 }
 
 func newResponseError(url string, resp *http.Response) error {
